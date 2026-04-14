be70ba01e5ef859697e1c84b62c6e6382dbec7e3
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-14 13:55:54 +0200

    Cambios en la ruta de inicio.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArchivoControlador;
 
-Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {return view('welcome');})->name('inicio');
 
 Route::post('/archivo', [ArchivoControlador::class, 'leer'])->name('archivo.leer');
 Route::get('/archivo', [ArchivoControlador::class, 'paginacion'])->name('archivo.paginacion');
