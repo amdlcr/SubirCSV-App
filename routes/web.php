19300e415c417b5fa94caea731fd37698a1b0b67
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-21 13:49:30 +0200

    Modificación de rutas.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArchivoControlador;
 
-Route::get('/', function () {return view('welcome');})->name('inicio');
+Route::get('/', function () {return view('index');})->name('inicio');
 
 Route::post('/archivo', [ArchivoControlador::class, 'leer'])->name('archivo.leer');
 Route::get('/archivo', [ArchivoControlador::class, 'paginacion'])->name('archivo.paginacion');
+
