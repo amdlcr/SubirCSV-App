04b9ad2355576bb4fd2c7b41205907f09c93240c
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-24 13:45:47 +0200

    Cambios en el selector y algunos botones

--- resources/views/visualizacionArchivo.blade.php
+++ resources/views/visualizacionArchivo.blade.php
@@ -8,22 +8,36 @@
         <link rel="stylesheet" href="{{ asset('css/pag_visualizacion_archivo.css') }}">
     </head>
     <body>
-         <a class="botonVolver" href="{{ route('inicio') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M268.2 82.4C280.2 87.4 288 99 288 112L288 192L400 192C497.2 192 576 270.8 576 368C576 481.3 494.5 531.9 475.8 542.1C473.3 543.5 470.5 544 467.7 544C456.8 544 448 535.1 448 524.3C448 516.8 452.3 509.9 457.8 504.8C467.2 496 480 478.4 480 448.1C480 395.1 437 352.1 384 352.1L288 352.1L288 432.1C288 445 280.2 456.7 268.2 461.7C256.2 466.7 242.5 463.9 233.3 454.8L73.3 294.8C60.8 282.3 60.8 262 73.3 249.5L233.3 89.5C242.5 80.3 256.2 77.6 268.2 82.6z"/></svg>
-         </a>
+        
+    
+    <form action="{{ route('cerrar.archivo') }}" method="POST">
+    @csrf
+        <input type="hidden" name="archivo" value="{{ $archivo }}">
+            <button type="submit" class="botonVolver">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M268.2 82.4C280.2 87.4 288 99 288 112L288 192L400 192C497.2 192 576 270.8 576 368C576 481.3 494.5 531.9 475.8 542.1C473.3 543.5 470.5 544 467.7 544C456.8 544 448 535.1 448 524.3C448 516.8 452.3 509.9 457.8 504.8C467.2 496 480 478.4 480 448.1C480 395.1 437 352.1 384 352.1L288 352.1L288 432.1C288 445 280.2 456.7 268.2 461.7C256.2 466.7 242.5 463.9 233.3 454.8L73.3 294.8C60.8 282.3 60.8 262 73.3 249.5L233.3 89.5C242.5 80.3 256.2 77.6 268.2 82.6z"/></svg>
+            </button>
+    </form>
 
         <h4>Datos del Archivo:</h4>
         <h2>{{ $nombreArchivo }}</h2>
 
-        <div class="opcionesVisualizacion">
-            <label for="opciones">Vista</label>
-                <select name="opciones" id="opciones" class="mi-selector">
-                    <option value="1">5</option>
-                    <option value="2">10</option>
-                    <option value="3">20</option>
-                    <option value="4">50</option>
+
+        <form method="GET">
+            <input type="hidden" name="archivo" value="{{ $archivo }}"> <!-- Asi le indicamos a la vista al navegar en que archivo estamos  -->
+             <label for="opcionesVista">Mostrar:</label>
+                <select name="opcionesVista"class="opcionesVista" onchange="this.form.submit()">
+                    @foreach([5, 10, 20, 50] as $vistas)
+                        <option value="{{ $vistas }}" {{ $filasPorPagina == $vistas ? 'selected' : '' }}>
+                            {{ $vistas }}
+                        </option>
+                    @endforeach
                 </select>
-        </div>
+        </form>
+
+
+
+
+
 
         <table class="tabla">
             <thead>
@@ -45,8 +59,11 @@
             </tbody>
         </table>
 
-        <br>
-
+      
+    <div class="barraInferior"> 
+        <p class="contadorFilas">
+            Registros: {{ $totalFilas }}
+        </p>
         <div class="paginacion">
             <div class="botonesPaginacion">
                 @if (!$datos->onFirstPage())
@@ -77,7 +94,8 @@
             </p>
             
         </div>
-       
+    </div>
+     
 
     </body>
 </html>
