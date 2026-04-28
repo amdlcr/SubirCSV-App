0c943051d6fc6c11df81244b1969b6b940bb2f20
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-28 13:28:07 +0200

    Refactorización de ArchivoControlador a CsvController.

---
+++ app/Http/Controllers/CsvController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CsvRequest; //Gestionamos la validacion aqui
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use App\Services\CsvService;
+use SplFileObject;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class CsvController extends Controller{
+
+    protected $csvService;
+
+    public function __construct(CsvService $csvService) {
+        $this->csvService = $csvService;
+    }
+
+//TENGO QUE DESCRIBIR LOS METODOS Y HACER SU DOMUMENTACION
+    public function leer(CsvRequest $request){
+        
+        $archivo= $request->file('anadirArchivo'); //Accedemos al archivo 
+        $nombreArchivo = $archivo->getClientOriginalName(); //Seleccionamos el nombre del archivo 
+        $archivoAlmacenado = $archivo->store('csv'); //Guardamos la ruta del archivo
+
+        return redirect()->route('archivo.mostrar',
+         ['archivo' => $archivoAlmacenado,
+          'nombreArchivo'=>$nombreArchivo
+         ]);
+    }
+
+    public function mostrar(Request $request){
+        $archivo = $request->get('archivo'); 
+        if (!Storage::exists($archivo)) {
+            return redirect()->route('index')->withErrors('Archivo no encontrado');
+        }
+
+        $nombreArchivo = $request->get('nombreArchivo', ''); //Recogemos el nombre del archivo que envio el metodo leer
+        
+        $resultado = $this->csvService->buscar(
+            $archivo, 
+            $request->get('inputBuscar'), 
+            $request->get('opcionesBuscar'),
+            $request->get('separador')
+        );
+        
+        if (is_null($resultado)) {
+            return redirect()->route('index')->withErrors('El archivo está vacío.');
+        }
+        
+        $todasLasFilas = $resultado['filas'];
+        array_shift($todasLasFilas);//limpia el primer registro del array
+        $totalFilas= count($todasLasFilas);
+        $paginaActual = LengthAwarePaginator::resolveCurrentPage();//   
+        $filasPorPagina = (int) $request->get('opcionesVista', 10); //numero de filas que se van a amostrar por pagina
+
+         //paginacion, solo se leen 10 paginas por linea
+        $inicio = ($paginaActual - 1) * $filasPorPagina;
+        $datosPaginados = array_slice($todasLasFilas, $inicio, $filasPorPagina);
+        
+        $paginador = new LengthAwarePaginator(  //Clase de laravel para paginar
+            $datosPaginados, 
+            $totalFilas, 
+            $filasPorPagina, 
+            $paginaActual, 
+            [
+                'path' => $request->url(),
+                'query' => $request->query(), // Esto mantiene 'archivo' y 'nombreArchivo' automaticamente,permite que el paginador "recuerde" los parametros
+            ]
+        );
+
+        $paginador->onEachSide(2); 
+       
+        //Enviamos la informacion a la vista donde se muestra
+        return view('visualizacionCsv', [
+            'columnas' => $resultado['columnas'],
+            'datos'=> $paginador,
+            'archivo' => $archivo,
+            'nombreArchivo'=> $nombreArchivo,
+            'totalFilas'=>$totalFilas,
+            'filasPorPagina'=>$filasPorPagina,
+            'separador' => $resultado['separador']
+        ]);
+    }
+
+    public function eliminarCsv(Request $request){
+        $archivo = $request->input('archivo');
+
+        if (Storage::exists($archivo)) {
+            Storage::delete($archivo);
+        }
+        return redirect()->route('index');
+    }
+
+}
