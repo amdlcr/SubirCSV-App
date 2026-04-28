9bd96e01337f968b21d61118f3b0d074851d5cc3
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-28 13:31:27 +0200

    Cambios en nombre de rutas.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArchivoControlador;
+use App\Http\Controllers\CsvController;
 
-Route::get('/', function () {return view('index');})->name('inicio');
+Route::get('/', function () {return view('index');})->name('index');
 
-Route::post('/archivo', [ArchivoControlador::class, 'leer'])->name('archivo.leer');
-Route::get('/archivo', [ArchivoControlador::class, 'paginacion'])->name('archivo.paginacion');
-Route::post('/cerrar', [ArchivoControlador::class, 'cerrarArchivo'])->name('cerrar.archivo');
+Route::post('/archivo', [CsvController::class, 'leer'])->name('archivo.leer');
+Route::get('/archivo', [CsvController::class, 'mostrar'])->name('archivo.mostrar');
+Route::post('/eliminar', [CsvController::class, 'eliminarCsv'])->name('eliminar.csv');
 
